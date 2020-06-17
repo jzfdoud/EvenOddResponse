3e6953c5ee1a9358df3e593050eef00d2f49a731
Jada <jada.fagan@gmail.com>
2020-06-17 15:40:16 -0400

    foreach example
    * practiced using foreach to find sum of random numbers
    * used random integers from random.org

--- PracticeEvenOdd/Program.cs
+++ PracticeEvenOdd/Program.cs
@@ -17,17 +17,27 @@ namespace PracticeEvenOdd
             //    Console.WriteLine($"The number {nbr} is odd");
             //}
 
-            var total = 0;
-            for (var idx = 0; idx <= 25; idx++)
-            {
-                total += idx; /* += instead of t = t +idx*/
-            }
-            Console.WriteLine($"The sum of 1 o 25 is {total}");
+
             // var nbr= int;
             //Console.ReadLine();
             //if (nbr = 2 % 2) {
             //    Console.WriteLine("Number is even");
             //    if (nbr != 2 % 2) { Console.WriteLine("Number is odd"); }
+
+
+            //var total = 0;
+            //for (var idx = 0; idx <= 25; idx++)
+            //{
+            //    total += idx; /* += instead of t = t +idx*/
+            //}
+            //Console.WriteLine($"The sum of 1 o 25 is {total}");
+
+            int[] nbrs = { 946, 881, 509, 535, 589 };
+            var total = 0;
+                foreach(var i in nbrs) {
+                    total += i;
+            }
+            Console.WriteLine($"The sum of random numbers is {total}");
             }
 
 
