7cbd0a47e19ebdda7a8b00f705d8f273b91061ed
Jada <jada.fagan@gmail.com>
2020-06-17 14:35:16 -0400

    Add project files.

---
+++ PracticeEvenOdd/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PracticeEvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter an integer : ");
+            var ans = Console.ReadLine();
+            var nbr = Convert.ToInt32(ans);
+            if (nbr % 2 == 0)
+            {
+                Console.WriteLine($"The number {nbr} is even");
+            } else {
+                Console.WriteLine($"The number {nbr} is odd");
+            }
+
+            // var nbr= int;
+            //Console.ReadLine();
+            //if (nbr = 2 % 2) {
+            //    Console.WriteLine("Number is even");
+            //    if (nbr != 2 % 2) { Console.WriteLine("Number is odd"); }
+            }
+
+
+        }
+    }
+
