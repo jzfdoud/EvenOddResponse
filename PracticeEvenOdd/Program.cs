3100a2fbf83bfc2198f5901c6a91b23419b781cd
Jada <jada.fagan@gmail.com>
2020-06-17 14:59:58 -0400

    example of for loop
    *used for loop
    *defined variable
    *used app ptp find the sum of the numbers 1-25

--- PracticeEvenOdd/Program.cs
+++ PracticeEvenOdd/Program.cs
@@ -7,16 +7,22 @@ namespace PracticeEvenOdd
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Enter an integer : ");
-            var ans = Console.ReadLine();
-            var nbr = Convert.ToInt32(ans);
-            if (nbr % 2 == 0)
-            {
-                Console.WriteLine($"The number {nbr} is even");
-            } else {
-                Console.WriteLine($"The number {nbr} is odd");
-            }
+            //Console.WriteLine("Enter an integer : ");
+            //var ans = Console.ReadLine();
+            //var nbr = Convert.ToInt32(ans);
+            //if (nbr % 2 == 0)
+            //{
+            //    Console.WriteLine($"The number {nbr} is even");
+            //} else {
+            //    Console.WriteLine($"The number {nbr} is odd");
+            //}
 
+            var total = 0;
+            for (var idx = 0; idx <= 25; idx++)
+            {
+                total += idx; /* += instead of t = t +idx*/
+            }
+            Console.WriteLine($"The sum of 1 o 25 is {total}");
             // var nbr= int;
             //Console.ReadLine();
             //if (nbr = 2 % 2) {
